e900a4af7d33a92dbbcda53b29c85511e9e4577f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-23 04:10:28 -0700

    wip

--- test/test.expected.ml
+++ test/test.expected.ml
@@ -1,6 +1,5 @@
-let x ~children  =
-  ((div ~id:(("omg")[@reason.raw_literal "omg"]) ~children:[] ())[@JSX ])
-  [@@react.component ]
+let x ~children  = ((div ~id:"omg" ~children:[] ())[@JSX ])[@@react.component
+                                                             ]
 let x ~some_prop  ~children  =
   ((div ~some_prop ~other_prop:some_prop ~children:[] ())[@JSX ])[@@react.component
                                                                    ]
