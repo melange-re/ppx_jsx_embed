5681288e0b4699ce95ae9789d6c10d0721a4463a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-04-23 04:40:30 -0700

    wip

--- nix/ci/test.nix
+++ nix/ci/test.nix
@@ -4,17 +4,7 @@ let
   flake = builtins.getFlake (builtins.unsafeDiscardStringContext ./../..);
   system = builtins.currentSystem;
   pkgs = flake.inputs.nixpkgs.legacyPackages."${system}".extend (self: super: {
-    ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}".overrideScope' (oself: osuper: {
-      reason = osuper.reason.overrideAttrs (o: {
-        src = super.fetchFromGitHub {
-          owner = "reasonml";
-          repo = "reason";
-          rev = "6401d10f2d1e2c8e1973c0de61d3c27d70b37248";
-          hash = "sha256-QJORWjqGuz6pLhZTFLhWxofDsa4dAquVtwhdbGrv0pE=";
-        };
-        propagatedBuildInputs = o.propagatedBuildInputs ++ (with oself; [ dune-build-info ppxlib ]);
-      });
-    });
+    ocamlPackages = super.ocaml-ng."ocamlPackages_${ocamlVersion}";
   });
 
   lock = builtins.fromJSON (builtins.readFile ./../../flake.lock);
